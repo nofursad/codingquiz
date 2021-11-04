fe021649fca9070c91d0515184133d2572d6e786
Abhisek Joshi <nofursad@gmail.com>
2021-11-04 15:27:19 +0000

    Define the required functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,45 @@
 //Wait for the DOM to finish loading before starting the Quiz
 document.addEventListener("DOMContentLoaded", function(){
-    let catagories = document.getElementsByClassName("category");
-
-    for (let category of catagories){
-        if(category.className.includes("btnActive")){
-            displayHtmlQuestion();
-        }
+    let buttons  = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            let quizCat = button.getAttribute("data-type");
+            resetCounter();
+            changeCategory(quizCat);
+            runQuiz(quizCat);
+        })
     }
+    runQuiz("html");
 })
+
+function runQuiz(quizCat){
+
+}
+
+function changeCategory(quizCat){
+}
+
+function checkAnswer(){
+}
+
+function increaseScore(){
+}
+
+function resetCounter(){
+}
+
+function increaseQueNum(){
+}
+
+function getCorrectAnswer(){       
+}
+
+function displayHtmlQuestion(queNum){
+    
+}
+
+function displayCssQuestion(queNum){
+}
+
+function displayJsQuestion(queNum){
+}
