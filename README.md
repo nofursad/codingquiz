5bab149857a5c46389ae1ba61102bebfa125a316
Abhisek Joshi <nofursad@gmail.com>
2021-11-06 13:43:42 +0000

    Update ReadMe file and made change to socing function in script.js

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Coding Quiz is a place to check the knowledge of a user in 3 specific coding language, **HTML**, **CSS** and **Javascript**. User of a website will have an option to chose from 3 different coding language they want to test their knowledge on. They will be asked 10 questions from the language they selected and at the end of 10 questions, it will display the score to the user. 
 
-##Demo
+## Demo
 
 A live demo of this website can be found [here](https://nofursad.github.io/codingquiz/)
 
@@ -12,8 +12,6 @@ A live demo of this website can be found [here](https://nofursad.github.io/codin
 
 ![Responsiveness]
 
-
-You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Gitpod and the extensions we use. Some of this information has been updated since the video content was created. The last update to this file was: **September 1, 2021**
 
 ## Gitpod Reminders
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,12 @@ document.addEventListener("DOMContentLoaded", function(){
     runQuiz("html");
 })
 
+// core function to run the quiz
 function runQuiz(quizCat){
 
 }
 
+// function to change the btnActive class in index page to hightlight the quiz category
 function changeCategory(quizCat){
     if (quizCat === "html"){
         document.getElementById("htmlBtn").className = "category btncategory btnActive";
@@ -35,48 +37,60 @@ function changeCategory(quizCat){
 function getCorrectAnswer(){       
 }
 
+//take the answer from user and compare the value to the result of
+//getCorrectAnswer function to determine if user's answer is correct of not
 function checkAnswer(){
 }
 
+//increase the score of user if the answer is correct
 function increaseScore(){
     let score = document.getElementById("score");
     score.innerText = parseInt(score.innerText) + 1;
 }
 
+//reset the question counter and score counter when user
+// select different catefory of quiz
+function resetCounter(){
+    document.getElementById("score").innerText = 0;
+    document.getElementById("questionNum").innerText = 1;
+    document.getElementById("min").innerText = 00;
+    document.getElementById("sec").innerText = 00;
+}
+
+// increase the question number value displayed at the 
+//info bar at the buttom of quiz after user answer the question
 function increaseQueNum(){
     let queNum = document.getElementById("questionNum");
     queNum.innerText = parseInt(queNum.innerText) + 1;
 }
 
-function resetCounter(){
-    document.getElementById("score").innerText = 0;
-    document.getElementById("questionNum").innerText = 1;
-}
-
-function displayHtmlQuestion(queNum){
+// display the html question when html category is selected.
+function displayhtmlQuestion(queNum){
     let questionDisplay = document.getElementById("question");
     let answersDisplay = document.getElementsByClassName("answers");
-    let queAndAns = htmlQue[queNum];
+    let queAndAns = html[queNum];
     questionDisplay.innerHTML = queAndAns.question;
     for (let x = 0; x < answersDisplay.length; x++){
         answersDisplay[x].innerHTML = queAndAns.answers[x].text;
     };
 }
 
+// display the html question when css category is selected.
 function displayCssQuestion(queNum){
     let questionDisplay = document.getElementById("question");
     let answersDisplay = document.getElementsByClassName("answers");
-    let queAndAns = cssQue[queNum];
+    let queAndAns = css[queNum];
     questionDisplay.innerHTML = queAndAns.question;
     for (let x = 0; x < answersDisplay.length; x++){
         answersDisplay[x].innerHTML = queAndAns.answers[x].text;
     };
 }
 
+// display the html question when javascript category is selected.
 function displayJsQuestion(queNum){
     let questionDisplay = document.getElementById("question");
     let answersDisplay = document.getElementsByClassName("answers");
-    let queAndAns = jsQue[queNum];
+    let queAndAns = javascript[queNum];
     questionDisplay.innerHTML = queAndAns.question;
     for (let x = 0; x < answersDisplay.length; x++){
         answersDisplay[x].innerHTML = queAndAns.answers[x].text;
