ec15e660990c6b081b888b1105acebd09941e78a
Abhisek Joshi <nofursad@gmail.com>
2021-11-08 13:10:45 +0000

    Declearing the required functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,36 @@
  let cssBtn = document.getElementById("cssBtn");
  let jsBtn = document.getElementById("jsBtn");
 
+ // Function to display HTML question when HTML category is selected
+function displayHtmlQue(queNumInt) {
+}
+
+// Function to display HTML question when HTML category is selected
+function displayCssQue(queNumInt) {
+}
+
+// Function to display HTML question when HTML category is selected
+function displayJsQue(queNumInt) {
+}
+
+/**
+ * Listen to the click on the answer buttons and get the data-type.
+ * Compares the data-type and give the response to user answer.
+ * Increase the question number and socre if answer is correct but
+ * only increase question number if answer is incorrect.
+ * After 10 questions, give full result to user and reset the quiz.
+ */
+ function checkAnswer() {
+ }
+
+ // Function to identify the category of the quiz that is selected buy user.
+ function queCatSelect(quizCat){
+ }
  
+ /**
+  * The main quiz function which connect other function together.
+  * Get the category of the quiz when the DOM is loaded and
+  * run the quiz category as per user request.
+  */
+ function runQuiz(quizCat) {
+ }
